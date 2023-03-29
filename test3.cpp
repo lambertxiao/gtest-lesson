ecc2d0e96c0012b416eb88d70ca46153478bd121
lambert.xiao <lambertxiao@ucloud.cn>
2023-03-30 02:57:00 +0800

    update case

--- test3.cpp
+++ test3.cpp
@@ -12,45 +12,53 @@ class IDiskUitl {
 class FileWriter {
  public:
   IDiskUitl* diskutil_;
+  uint64_t offset_;
 
-  FileWriter(IDiskUitl *diskutil) : diskutil_(diskutil) {}
+  FileWriter(IDiskUitl *diskutil, uint64_t offset) : diskutil_(diskutil), offset_(offset) {}
 
-  std::string writeFile(int offset, char* data, int len) {
-    int ret = diskutil_->write(offset, data, len);
-    return ret == 0 ? "success" : "error";
+  int writeFile(char* data, int len) {
+    int ret = diskutil_->write(offset_, data, len);
+    if (ret == 0) {
+      offset_ += len;
+    }
+    return ret;
   }
 };
 
 class MockDiskUtil : public IDiskUitl {
  public:
   MOCK_METHOD(int, write, (int, char*, int), (override));
-}; 
+};
 
 class FileWriterTestSuite : public testing::Test {
  protected:
   // 每个case执行前都会调用
-  void SetUp() override {}  
+  void SetUp() override {}
 
   StrictMock<MockDiskUtil> diskutil_;
 };
 
 TEST_F(FileWriterTestSuite, testWriteSuccess) {
-  FileWriter w(&diskutil_);
+  uint64_t woff = 1024;
+  FileWriter w(&diskutil_, woff);
 
   std::string data = "hello";
-  EXPECT_CALL(diskutil_, write(1024, (char*)data.c_str(), data.length())).WillOnce(Return(0));
+  EXPECT_CALL(diskutil_, write(woff, (char*)data.c_str(), data.length())).WillOnce(Return(0));
 
-  std::string ret = w.writeFile(1024, (char*)data.c_str(), data.length());
-  EXPECT_EQ(ret, "success");
+  int ret = w.writeFile((char*)data.c_str(), data.length());
+  EXPECT_EQ(ret, 0);
+  EXPECT_EQ(w.offset_, woff + data.length());
 }
 
 TEST_F(FileWriterTestSuite, testWriteFailed) {
-  FileWriter w(&diskutil_);
+  uint64_t woff = 1024;
+  FileWriter w(&diskutil_, woff);
 
   std::string data = "hello";
-  EXPECT_CALL(diskutil_, write(1024, (char*)data.c_str(), data.length())).WillOnce(Return(-1));
+  EXPECT_CALL(diskutil_, write(woff, (char*)data.c_str(), data.length())).WillOnce(Return(1));
 
-  std::string ret = w.writeFile(1024, (char*)data.c_str(), data.length());
-  EXPECT_EQ(ret, "error");
+  int ret = w.writeFile((char*)data.c_str(), data.length());
+  EXPECT_EQ(ret, 1);
+  EXPECT_EQ(w.offset_, woff);
 }
 
